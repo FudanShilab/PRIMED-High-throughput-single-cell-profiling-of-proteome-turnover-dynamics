5bab1c87c872d70d9bc6d65de6a2dfd0dca837a8
Shilab <mqwei23@m.fudan.edu.cn>
2025-11-08 14:16:31 +0800

    Update data_convert.m

--- data_convert.m
+++ data_convert.m
@@ -1,4 +1,7 @@
 %% =========================================================================
+% Copyright (c) 2025 Lixue Shi and collaborators
+% Licensed under the Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
+%
 % Script Name: data_convert.m
 % Description:
 %   Convert a single FTIR imaging matrix stored in ASCII .dat format into a
